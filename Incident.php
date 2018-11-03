82ee2cabf0d9107763f13f112bf598a43aebc799
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-03 18:52:57 -0400

    Convert Category and Urgency to selection box

--- Incident.php
+++ Incident.php
@@ -1,38 +1,3 @@
-<?php session_start(); ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Incident</title>
-    </head>
-    <body>
-    Logged in as: <?php echo $_SESSION['Username']; ?> | <a href="logout.php">Log out</a>
-
-    <form method="post">
-        <label for="Description">Description</label>
-        <br>
-        <input type="text" id="Description" name="Des">
-        <br>
-        <label for="Category">Category</label><br>
-        <input type="text" id="Category" name="Category">
-        <br>
-        <label for="Urgency">Urgency</label>
-        <br>
-        <input type="text" id="Urgency" name="Urgency"/>
-        <br>
-        <label for="Location">Location</label>
-        <br>
-        <input type="text" id="Location" name="Location"/>
-        <br>
-        <input type="submit" value="Submit" name="Submit"/>
-
-
-    </form>
-
-    </body>
-    </html>
-
-
 <?php
 /**
  * Created by PhpStorm.
@@ -40,6 +5,9 @@
  * Date: 2018-11-01
  * Time: 2:47 PM
  */
+
+session_start();
+
 
 if (!isset($_SESSION['Username'])) {
     header('Location:restricted.php');
@@ -56,15 +24,13 @@ or die("Connection Failed");
 
 if (!empty($_POST['Category'])) {
     $Category = $_POST["Category"];
-    $Category = mysqli_real_escape_string($connect, $Category);
 
-    $query = "SELECT incident_name from incidentcategory where incident_name = '$Category'";
+    $query = "SELECT incident_id, incident_name from incidentcategory where incident_name = '$Category'";
     $result = mysqli_query($connect, $query);
 
 
     if ($result) {
         $Urgency = $_POST["Urgency"];
-        $Urgency = mysqli_real_escape_string($connect, $Urgency);
 
         $Location = $_POST["Location"];
         $Location = mysqli_real_escape_string($connect, $Location);
@@ -76,7 +42,6 @@ if (!empty($_POST['Category'])) {
         $UserRec = mysqli_real_escape_string($connect, $UserRec);
 
         $Category = $_POST["Category"];
-        $Category = mysqli_real_escape_string($connect, $Category);
 
         $Date = date("Y-m-d H:i:s", time());
         $Date = mysqli_real_escape_string($connect, $Date);
@@ -97,3 +62,56 @@ if (!empty($_POST['Category'])) {
 
 }
 
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Incident</title>
+    </head>
+    <body>
+    Logged in as: <?php echo $_SESSION['Username']; ?> | <a href="logout.php">Log out</a>
+
+    <form method="post">
+        <label for="Description">Description</label>
+        <br>
+        <input type="text" id="Description" name="Des">
+        <br>
+
+        <label for="Category">Category</label><br>
+        <select id="Category" name="Category">
+            <?php
+
+            $selectQuery = "SELECT incident_name FROM incidentcategory";
+
+            $selectResult = mysqli_query($connect, $selectQuery) or die("query is failed" . mysqli_error($con));
+
+            while (($row = mysqli_fetch_row($selectResult)) == true) {
+                echo "<option value='$row[0]'>$row[0]</option>";
+            };
+
+            ?>
+        </select>
+        <br>
+        <label for="Urgency">Urgency</label>
+        <br>
+        <select id="Urgency" name="Urgency">
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+            <option value="very high">Very High</option>
+        </select>
+        <br>
+        <label for="Location">Location</label>
+        <br>
+        <input type="text" id="Location" name="Location"/>
+        <br>
+        <input type="submit" value="Submit" name="Submit"/>
+
+
+    </form>
+
+    </body>
+    </html>
