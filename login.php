cef132454cfa6d3f9d1d5b7f74f3a1b105675981
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-03 00:43:23 -0400

    Sessions added

--- login.php
+++ login.php
@@ -1,36 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php session_start(); ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Search Form</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Log in</title>
+    </head>
 
-<body>
-<form method="post">
-    <p>
-        <input type="radio" name="Job" id="Employee" value="Employee" checked="checked">
-        <label for="Employee">Employee</label>
-        <input type="radio" name="Job" id="Guest" value="Guest">
-        <label for="Guest">Guest</label>
-    </p>
-    <p>
-        <label for="Login">Username:</label>
-        <br>
-        <input type="text" id="Login" name="User" />
-        <br>
-        <label for="Password">Password:</label>
-        <br>
-        <input type="text" id="Password" name="Pass" />
-    </p>
-    <input type="submit" value="Login" name="Login" />
-    <input type="button" onclick="location.href='Signup.php';" value="Guest Signup" />
+    <body>
+    <?php if (isset($_GET['logout']) == 1) echo "You have logged out"; ?>
+    <form method="post">
+        <p>
+            <input type="radio" name="Job" id="Employee" value="Employee" checked="checked">
+            <label for="Employee">Employee</label>
+            <input type="radio" name="Job" id="Guest" value="Guest">
+            <label for="Guest">Guest</label>
+        </p>
+        <p>
+            <label for="Login">Username:</label>
+            <br>
+            <input type="text" id="Login" name="User"/>
+            <br>
+            <label for="Password">Password:</label>
+            <br>
+            <input type="text" id="Password" name="Pass"/>
+        </p>
+        <input type="submit" value="Login" name="Login"/>
+        <input type="button" onclick="location.href='Signup.php';" value="Guest Signup"/>
 
-</form>
+    </form>
 
-</body>
+    </body>
 
-</html>
+    </html>
 
 
 <?php
@@ -40,6 +42,7 @@
  * Date: 2018-11-01
  * Time: 2:23 PM
  */
+
 $host = "localhost";
 $user = "root";
 $password = "";
@@ -51,13 +54,24 @@ if (!empty($_POST['User'])) {
         $Pass = $_POST["Pass"];
         $connect = mysqli_connect($host, $user, $password, $dbName) or die("Connection Failed");
         if (isset($_POST['Login'])) {
-            $Query = "Select emp_id from user_info WHERE emp_id ='$Login' AND password = '$Pass'";
+            $Query = "Select emp_id, admin, name from user_info WHERE emp_id ='$Login' AND password = '$Pass'";
             $result = mysqli_query($connect, $Query);
             $row = mysqli_fetch_row($result);
             if (mysqli_num_rows($result) == 1 && $row[0]) {
-                header("Location:Incident.php?Job=Employee");
-            }
-            else Echo "Login Failed";
+                $_SESSION[`Login`] = $Login;
+                if ($row[1] == 1) {
+                    $_SESSION['Admin'] = true;
+                    $_SESSION['Username'] = $row[2];
+                    $_SESSION['Employee'] = false;
+                    header('Location:adminconsole.php');
+
+                } else {
+                    $_SESSION['Employee'] = true;
+                    $_SESSION['Username'] = $row[2];
+                    header("Location:Incident.php?Job=Employee");
+                }
+
+            } else Echo "Login Failed";
         }
     }
 
@@ -70,8 +84,7 @@ if (!empty($_POST['User'])) {
             $result = mysqli_query($connect, $Query);
             if (mysqli_num_rows($result) == 1) {
                 header("Location:Incident.php?Job=guest");
-            }
-            else Echo "Login Failed";
+            } else Echo "Login Failed";
         }
     }
 }
