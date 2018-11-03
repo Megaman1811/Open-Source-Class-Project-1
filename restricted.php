4b56670e19388c697ddeb9c5fa91b9b3d1ce5403
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-03 16:32:06 -0400

    Added checking for Session Cookies in restricted.php

--- restricted.php
+++ restricted.php
@@ -1,14 +1,15 @@
+<?php session_start(); ?>
 <html>
 <head>
-
+    <title>Access Denied</title>
 </head>
 <body>
+<?php echo $_SESSION['Username']; ?>
 <h1>You are not allowed on this page</h1>
-<?php if (!isset($_SESSION['Username'])) {
+<?php if (!isset($_SESSION['Login'])) {
     echo "<p>You will be redirected to the login page in 5 seconds.</p>";
     echo "<meta http-equiv=\"refresh\" content=\"5;url=login.php\"/>"; //Refreshes page in 5 seconds
-}
-if (isset($_SESSION['Username']) && !isset($_SESSION['Admin'])) {
+} else if (!isset($_SESSION['Admin'])) {
     echo "<p>You will be redirected to your homepage in 5 seconds.</p>";
     echo "<meta http-equiv=\"refresh\" content=\"5;url=login.php\"/>";
 }
