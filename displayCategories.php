1fb6c2a7caca60b02a5bbd5ea096291ea44122da
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-03 22:12:58 -0400

    Adding Logout button to pages

--- displayCategories.php
+++ displayCategories.php
@@ -1,3 +1,13 @@
+<?php session_start(); ?>
+<html>
+<head>
+
+</head>
+<body>
+Logged in as: <?php echo $_SESSION['Username']; ?> | <a href="logout.php">Log out</a><br>
+</body>
+</html>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +15,7 @@
  * Date: 2018-11-03
  * Time: 8:59 PM
  */
+
+if (!isset($_SESSION['Admin'])) {
+    header('Location:restricted.php');
+}
--- displayUsers.php
+++ displayUsers.php
@@ -1,3 +1,19 @@
+<?php session_start(); ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Display Users</title>
+    </head>
+    <body>
+    Logged in as: <?php echo $_SESSION['Username']; ?> | <a href="logout.php">Log out</a><br>
+    <?php if (isset($_GET['employeeCreated']) == 1) echo "Employee Created"; ?>
+
+    </body>
+
+
+    </html>
 <?php
 /**
  * Created by PhpStorm.
