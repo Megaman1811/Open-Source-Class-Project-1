1549e9bfe8d74fa9817c71f259ccb51583b2ffdc
Cristian Cerroni <cristian.cerroni@gmail.com>
2018-11-03 21:00:41 -0400

    Created pages for admin tools

---
+++ displayCategories.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ccerr
+ * Date: 2018-11-03
+ * Time: 8:59 PM
+ */
---
+++ makeEmployee.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ccerr
+ * Date: 2018-11-03
+ * Time: 8:58 PM
+ */
